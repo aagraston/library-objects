10b7332242713cdea63552d1c3d39ef25ff324c0
aagraston <aagraston@gmail.com>
2022-01-26 13:14:17 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,121 @@
+//constructor for book objects
+
+const booksContainer = document.querySelector('.books-container');
+
+const createButton = document.querySelector('#create-book');
+
+const bookFormContainer = document.querySelector('.new-book-form-container');
+const bookForm = document.querySelector('#book-form');
+
+createButton.addEventListener('click', makeNewBook);
+bookForm.elements["bsubmit"].addEventListener('click', parseFormData);
+
+let myLibrary = [];
+
+//array that houses books
+
+function makeNewBook() {
+  bookFormContainer.classList.toggle('show-form');
+  bookForm.elements["btitle"].focus();
+}
+
+function Book(title, author, description, read) {
+  this.title = title;
+  this.author = author;
+  this.description = description;
+  this.read = read;
+}
+
+Book.prototype.toggleRead = function() {
+  console.log(this.read);
+  if (this.read === "false") {
+    this.read = "true";
+  }
+  else {
+    this.read = "false";
+  }
+}
+
+function initToggleRead() {
+  console.log(this);
+let bookToToggle = myLibrary[parseInt(this.getAttribute("data-index"))];
+console.log(bookToToggle);
+bookToToggle.toggleRead(); 
+updateBooks();
+}
+
+function deleteBook() {
+  myLibrary.splice([parseInt(this.getAttribute("data-index"))], 1);
+  updateBooks();
+}
+
+function parseFormData(e) {
+
+  e.preventDefault();
+
+  let bookIsRead = "false";
+  if (bookForm.elements["bread"].checked) {
+    bookIsRead = "true";
+  }
+  let bookToAdd = new Book(
+    bookForm.elements["btitle"].value,
+    bookForm.elements["bauthor"].value,
+    bookForm.elements["bdescription"].value,
+    bookIsRead);
+
+  addBookToLibrary(bookToAdd);
+
+  bookForm.reset();
+  bookFormContainer.classList.toggle('show-form')
+}
+
+function addBookToLibrary(bookToAdd) {
+  myLibrary.push(bookToAdd);
+
+  updateBooks();
+}
+
+function updateBooks() {
+  booksContainer.innerHTML = '';
+
+  myLibrary.forEach((element, index) => {
+
+    let bookContainer = document.createElement('div');
+    bookContainer.classList.add('book');
+
+    let bookTitle = document.createElement('p');
+    bookTitle.innerText = "Title: " + element.title
+
+    let bookAuthor = document.createElement('p');
+    bookAuthor.innerText = "Author: " + element.author;
+
+    let bookDescription = document.createElement('p');
+    bookDescription.innerText = "Description: " + element.description;
+
+    let bookIsRead = document.createElement('p');
+    bookIsRead.innerText = "Have read: " + element.read;
+
+    let readToggle = document.createElement('button');
+    readToggle.innerText = "Read Toggle";
+    readToggle.setAttribute("data-index", index.toString());
+
+    readToggle.addEventListener('click', initToggleRead);
+
+
+    let delBook = document.createElement('button');
+    delBook.innerText = "Delete Book";
+    delBook.setAttribute("data-index", index.toString());
+
+    delBook.addEventListener('click', deleteBook);
+
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookDescription);
+    bookContainer.appendChild(bookIsRead);
+    bookContainer.appendChild(readToggle);
+    bookContainer.appendChild(delBook);
+    booksContainer.appendChild(bookContainer);
+
+  })
+}
